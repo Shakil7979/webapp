4e68dffe937566045cf2bcb758bdd4a8f7d43e0d
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-10-22 18:51:18 +0600

    update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,11 +4,19 @@ $(document).ready(function(){
     });
     $(".user_click").click(function(){
       $('.chat_popup_area').show(300);
+      $('.user_control_left ul li img').css({'transform':'rotate(180deg)'}); 
     });
     $(".show_hide").click(function(){
       $('.chat_popup_area').hide(300);
+      $('.user_control_left ul li img').css({'transform':'rotate(0deg)'}); 
     });
     $(".show_hide").click(function(){
       $('.user_click ul').show(300);
+    });
+    $(".settings_btn").click(function(){
+      $('.strim_setting_area').show();
+    });
+    $(".cencel_popup").click(function(){
+      $('.strim_setting_area').hide();
     });
   });
